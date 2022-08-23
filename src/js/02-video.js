e8c9a48a006c0d2a3704d0d54517d84d45d42699
Elena Lomakina <lomakina0612@gmail.com>
2022-08-23 15:26:11 +0200

    add checking timeFromStorage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,18 +7,19 @@ import storageApi from "./storage";
 const PLAYER_CURRENT_TIME = "videoplayer-current-time";
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const timeFromStorage = storageApi.load(PLAYER_CURRENT_TIME);
 
 // событие 'timeupdate' = {seconds: 116.658, percent: 0.204, duration: 571.563} 
 // throttle вызывает функцию handleTime не чаще чем 1 раз в сек(1000 мсек)
 player.on('timeupdate', throttle(handleTime, 1000));
 
-// берем из хранилища время и устанавливает его текущим значением перед началом воспроизведения
-player.setCurrentTime(storageApi.load(PLAYER_CURRENT_TIME));
+// берем из хранилища время и устанавливаем его текущим значением перед началом воспроизведения
+if (timeFromStorage) {
+  player.setCurrentTime(timeFromStorage);
+}
 
 
 //записывает в хранилище количество секунд воспроизведения
 function handleTime(event) {
   storageApi.save(PLAYER_CURRENT_TIME, event.seconds);
 }
-
-
